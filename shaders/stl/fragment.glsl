1f13a5484f9178ca08eef7e472b241ca49bb2e4d
Shane Williams <shanethomaswilliams@gmail.com>
2020-04-01 01:21:32 -0230

    Add ambient, diffuse and specular lighting capabilities to shaders

--- shaders/stl/fragment.glsl
+++ shaders/stl/fragment.glsl
@@ -1,11 +1,43 @@
-#version 330
-out vec4 FragColor;
+#version 330 core
 
-in vec2 TexCoord;
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
 
-uniform sampler2D texture_diffuse1;
+struct Light {  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    vec3 position;
+};
+
+in vec3 frag_pos;  
+in vec3 frag_norm;
+
+out vec4 frag_color;  
+
+uniform Material material;
+uniform Light light;
+uniform vec3 view_pos;
 
 void main()
-{    
-    FragColor = texture(texture_diffuse1, TexCoord);
+{
+    vec3 ambient = light.ambient * material.ambient;
+
+    vec3 unit_frag_norm = normalize(frag_norm);
+    vec3 light_dir = normalize(light.position - frag_pos);  
+    float diffuse_mag = max(dot(unit_frag_norm, light_dir), 0.0);
+    vec3 diffuse = light.diffuse * (diffuse_mag * material.diffuse);
+
+    vec3 view_dir = normalize(view_pos - frag_pos);
+    vec3 reflect_dir = reflect(light_dir, unit_frag_norm);  
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
+    
+    vec3 result = ambient + diffuse + specular;
+
+    frag_color = vec4(result, 1.0);
 }
--- shaders/stl/vertex.glsl
+++ shaders/stl/vertex.glsl
@@ -1,11 +1,9 @@
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoord;
+#version 330
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
 
-out vec3 Normal;
-out vec3 FragPos;  
-out vec2 TexCoord;
+out vec3 frag_pos;  
+out vec3 frag_norm;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,6 +11,7 @@ uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    TexCoord = aTexCoord;
+    gl_Position = projection * view * model * vec4(position, 1.0);
+    frag_pos = vec3(model * vec4(position, 1.0));
+    frag_norm = mat3(transpose(inverse(model))) * normal;  
 }
